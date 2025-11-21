8a3457021edb76f1bfd87817f43c12621eff3870
YodHeVauHe <mugharold@gmail.com>
2025-11-21 14:41:40 +0300

    Fix TypeScript error: unused parameter in letter-glitch component

--- src/components/ui/letter-glitch.tsx
+++ src/components/ui/letter-glitch.tsx
@@ -55,7 +55,7 @@ const LetterGlitch: React.FC<LetterGlitchProps> = ({
 
     const hexToRgb = (hex: string) => {
         const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-        hex = hex.replace(shorthandRegex, (m, r, g, b) => {
+        hex = hex.replace(shorthandRegex, (_m, r, g, b) => {
             return r + r + g + g + b + b;
         });
         const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
